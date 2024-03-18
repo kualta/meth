7cecb5a459750a46ef034b2d319e12c6f456502f
kualta <contact@kualta.dev>
2024-03-18 15:17:14 +0300

    send magic internet money

--- solana/src/app/layout.tsx
+++ solana/src/app/layout.tsx
@@ -1,21 +1,32 @@
+"use client";
+import {
+	ConnectionProvider,
+	WalletProvider,
+} from "@solana/wallet-adapter-react";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { useMemo } from "react";
+import * as web3 from "@solana/web3.js";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-	title: "solana",
-	description: "solana elements",
-};
+// export const metadata: Metadata = {
+// 	title: "solana",
+// 	description: "solana elements",
+// };
 
 export default function RootLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
+	const endpoint = web3.clusterApiUrl("devnet");
+	const wallets = useMemo(() => [], []);
+
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body>
+				<ConnectionProvider endpoint={endpoint}>
+					<WalletProvider wallets={wallets}>{children}</WalletProvider>
+				</ConnectionProvider>
+			</body>
 		</html>
 	);
 }
--- solana/src/app/page.tsx
+++ solana/src/app/page.tsx
@@ -1,22 +1,51 @@
 "use client";
-import { NextPage } from "next";
 import { FC, ReactNode, useMemo } from "react";
 import {
 	ConnectionProvider,
 	WalletProvider,
+	useConnection,
+	useWallet,
 } from "@solana/wallet-adapter-react";
 import * as web3 from "@solana/web3.js";
+import {
+	WalletModalProvider,
+	WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import "@solana/wallet-adapter-react-ui/styles.css";
 
 const Page = () => {
-	const endpoint = web3.clusterApiUrl("devnet");
-	const wallets = useMemo(() => [], []);
+	const { publicKey, sendTransaction } = useWallet();
 
+	const { connection } = useConnection();
+
+	const sendSol = (event) => {
+		event.preventDefault();
+
+		const transaction = new web3.Transaction();
+
+		const recipientPubKey = new web3.PublicKey(event.target.recipient.value);
+
+		const sendSolInstruction = web3.SystemProgram.transfer({
+			fromPubkey: publicKey,
+			toPubkey: recipientPubKey,
+			lamports: web3.LAMPORTS_PER_SOL * 0.1,
+		});
+
+		transaction.add(sendSolInstruction);
+
+		sendTransaction(transaction, connection).then((sig) => {
+			console.log(sig);
+		});
+	};
 	return (
-		<ConnectionProvider endpoint={endpoint}>
-			<WalletProvider wallets={wallets}>
-				<p>Put the rest of your app here</p>
-			</WalletProvider>
-		</ConnectionProvider>
+		<WalletModalProvider>
+			<WalletMultiButton />
+			<form onSubmit={sendSol}>
+				<input type="text" name="recipient" placeholder="Recipient" />
+
+				<button>Send 0.1 SOL</button>
+			</form>
+		</WalletModalProvider>
 	);
 };
 
